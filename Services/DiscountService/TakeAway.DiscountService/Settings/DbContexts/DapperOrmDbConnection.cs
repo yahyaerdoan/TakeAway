42e34f23319413b65794ac9a44e22b4c93d5c063
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-11 18:06:38 -0500

    Orm connection updated.

--- Services/DiscountService/TakeAway.DiscountService/Settings/DbContexts/DapperOrmDbConnection.cs
+++ Services/DiscountService/TakeAway.DiscountService/Settings/DbContexts/DapperOrmDbConnection.cs
@@ -5,8 +5,7 @@ namespace TakeAway.DiscountService.Settings.DbContexts;
 
 public class DapperOrmDbConnection(IConfiguration configuration)
 {
-    private readonly IConfiguration _configuration = configuration;
-    private readonly string _connectionString = configuration.GetConnectionString("DapperConnection");
-
+    private readonly string _connectionString = configuration.GetConnectionString("DapperConnection") 
+        ?? throw new InvalidOperationException("The ConnectionString property has not been initialized.");
     public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
 }
