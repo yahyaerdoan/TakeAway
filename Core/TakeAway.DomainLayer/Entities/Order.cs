c054502efd1278fd6aabe97ec5ce18b343f6f8f5
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-12 09:12:53 -0500

    Entities created.

---
+++ Core/TakeAway.DomainLayer/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace TakeAway.DomainLayer.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<OrderDetail> OrderDetails { get; set; }
+}
