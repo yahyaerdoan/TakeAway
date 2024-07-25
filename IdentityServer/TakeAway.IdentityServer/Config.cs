cf668b298bb73125f7e254af1b08d8ffbca64c5d
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-25 07:31:22 -0500

    Identity server config developed.

--- IdentityServer/TakeAway.IdentityServer/Config.cs
+++ IdentityServer/TakeAway.IdentityServer/Config.cs
@@ -2,57 +2,72 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
-namespace TakeAway.IdentityServer
+namespace TakeAway.IdentityServer;
+
+public static class Config
 {
-    public static class Config
-    {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+    public static IEnumerable<ApiResource> ApiResources =>
+    [
+       new("ResourceCatalog1"){Scopes = {"CatalogFullPermission"}},
+       new ("ResourceCatalog2"){Scopes = {"CatalogReadPermission"}},
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
+       new ("ResourceOrder"){Scopes = {"OrderFullPermission"}},
+       new ("ResourceDiscount"){Scopes = {"DiscountFullPermission"}},
+       new ("ResourceCargo"){Scopes = {"CargoFullPermission"}},
+       new ("ResourceComment"){Scopes = {"CommentFullPermission"}},
+       new ("ResourceBasket"){Scopes = {"BasketFullPermission"}},
+       new ("ResourceMessage"){Scopes = {"MessageFullPermission"}},
+       new ("ResourceOselot"){Scopes = {"OselotFullPermission"}},
+       new (IdentityServerConstants.LocalApi.ScopeName)
+    ];
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
-            {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+    public static IEnumerable<IdentityResource> IdentityResources =>
+    [
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+        new IdentityResources.Email(),
+    ];
+    public static IEnumerable<ApiScope> ApiScopes =>
+    [
+        new ("CatalogFullPermission", "Full Authority For  Catolog Operations"),
+        new ("CatalogReadPermission", "Read Authority For  Catolog Operations"),
+        new ("OrderFullPermission", "Full Authority For  Order Operations"),
+        new ("DiscountFullPermission", "Full Authority For  Discount Operations"),
+        new ("CargoFullPermission", "Full Authority For  Cargo Operations"),
+        new ("CommentFullPermission", "Full Authority For  Comment Operations"),
+        new ("BasketFullPermission", "Full Authority For  Basket Operations"),
+        new ("MessageFullPermission", "Full Authority For  Message Operations"),
+        new ("OselotFullPermission", "Full Authority For  Oselot Operations"),
+        new (IdentityServerConstants.LocalApi.ScopeName)
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
-    }
+    ];
+    public static IEnumerable<Client> Clients =>
+    [
+        new() {
+            ClientId= "TakeAwayVisitorId",
+            ClientName= "Take Away Visitor User",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = {new Secret("takeawaysecret".Sha256())},
+            AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", IdentityServerConstants.LocalApi.ScopeName },
+            AllowAccessTokensViaBrowser = true,
+        },
+        new(){
+           ClientId = "TakeAwayAdminId",
+           ClientName= "Take Away Visitor Admin User",
+           AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+           ClientSecrets = {new Secret("takeawaysecret".Sha256())},
+           AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "OrderFullPermission", "DiscountFullPermission",
+                "CargoFullPermission", "CommentFullPermission,", "BasketFullPermission", "MessageFullPermission", "OselotFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                IdentityServerConstants.StandardScopes.Email,
+            },
+            AccessTokenLifetime = 6000
+        }
+    ];
 }
