e50d3224c2f9922122e7fc2acd030fa13a136ae1
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-30 13:20:06 -0500

    Index created.

--- Frontends/TakeAway.UserInterface/Controllers/HomeController.cs
+++ Frontends/TakeAway.UserInterface/Controllers/HomeController.cs
@@ -2,31 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TakeAway.UserInterface.Models;
 
-namespace TakeAway.UserInterface.Controllers
+namespace TakeAway.UserInterface.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();
     }
 }
---
+++ Frontends/TakeAway.UserInterface/Dtos/CatalogServices/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TakeAway.UserInterface.Dtos.CatalogServices.ProductDtos;
+
+public class ResultProductDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string MainDescription { get; set; }
+    public string SubDescription { get; set; }
+    public string Price { get; set; }
+    public string ImageUrl { get; set; }
+}
