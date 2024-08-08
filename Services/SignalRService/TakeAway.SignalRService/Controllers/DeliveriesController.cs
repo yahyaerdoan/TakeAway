2b9bd4e83561dc141a560d307b65c2b272762656
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-08-08 06:45:10 -0500

    Db and api tested.

---
+++ Services/SignalRService/TakeAway.SignalRService/Controllers/DeliveriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TakeAway.SignalRService.Entities;
+using TakeAway.SignalRService.Settings.DbContexts;
+
+namespace TakeAway.SignalRService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveriesController(SignalRServiceDeliveryMsSqlDbContext dbContext) : ControllerBase
+    {
+        private readonly SignalRServiceDeliveryMsSqlDbContext dbContext = dbContext;
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Delivery delivery)
+        {
+          dbContext.Deliveries.Add(delivery);
+          await  dbContext.SaveChangesAsync();
+           return Ok("created!");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+          var values = await dbContext.Deliveries.ToListAsync();
+           
+            return Ok(values);
+        }
+    }
+}
